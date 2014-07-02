98f9ac1fb883aaa61c89f50716d135db336f2586
Tyler Treat <tyler.treat@webfilings.com>
2014-07-02 17:51:41 -0500

    Make command-line interface for running tests

--- main.go
+++ main.go
@@ -1,35 +1,77 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"os"
+	"strconv"
 
 	"github.com/tylertreat/mq-benchmarking/benchmark"
 	"github.com/tylertreat/mq-benchmarking/benchmark/mq"
 )
 
-func runInproc() {
-	fmt.Println("Begin inproc test")
-	const messageCount = 1000000
-	const messageSize = 1000
-
+func runInproc(messageCount int, messageSize int) {
+	log.Println("Begin inproc test")
 	inproc := mq.NewInproc(messageCount)
+	inproc.Setup()
 	benchmark.Runner{inproc, inproc}.Run(messageSize, messageCount)
-	fmt.Println("End inproc test")
+	inproc.Teardown()
+	log.Println("End inproc test")
 }
 
-func runZeromq() {
-	fmt.Println("Begin zeromq test")
-	const messageCount = 1000000
-	const messageSize = 1000
-
+func runZeromq(messageCount int, messageSize int) {
+	log.Println("Begin zeromq test")
 	zeromq := mq.NewZeromq(messageCount)
 	zeromq.Setup()
 	benchmark.Runner{zeromq, zeromq}.Run(messageSize, messageCount)
 	zeromq.Teardown()
-	fmt.Println("End zeromq test")
+	log.Println("End zeromq test")
+}
+
+func runNanomsg(messageCount int, messageSize int) {
+	log.Println("Begin nanomsg test")
+	nanomsg := mq.NewNanomsg(messageCount)
+	nanomsg.Setup()
+	benchmark.Runner{nanomsg, nanomsg}.Run(messageSize, messageCount)
+	nanomsg.Teardown()
+	log.Println("End nanomsg test")
 }
 
 func main() {
-	runInproc()
-	runZeromq()
+	if len(os.Args) < 2 {
+		log.Printf("usage: %s inproc|zeromq|nanomsg [num_messages] [message_size]", os.Args[0])
+		os.Exit(1)
+	}
+
+	test := os.Args[1]
+	messageCount := 1000000
+	messageSize := 1000
+
+	if len(os.Args) > 2 {
+		count, err := strconv.Atoi(os.Args[2])
+		if err != nil {
+			log.Printf("usage: %s inproc|zeromq|nanomsg [num_messages] [message_size]", os.Args[0])
+			os.Exit(1)
+		}
+		messageCount = count
+	}
+
+	if len(os.Args) > 3 {
+		size, err := strconv.Atoi(os.Args[3])
+		if err != nil {
+			log.Printf("usage: %s inproc|zeromq|nanomsg [num_messages] [message_size]", os.Args[0])
+			os.Exit(1)
+		}
+		messageSize = size
+	}
+
+	if test == "inproc" {
+		runInproc(messageCount, messageSize)
+	} else if test == "zeromq" {
+		runZeromq(messageCount, messageSize)
+	} else if test == "nanomsg" {
+		runNanomsg(messageCount, messageSize)
+	} else {
+		log.Printf("usage: %s inproc|zeromq|nanomsg [num_messages] [message_size]", os.Args[0])
+		os.Exit(1)
+	}
 }
